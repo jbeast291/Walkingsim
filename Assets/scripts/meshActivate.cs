06ffdb02ce66386932769ec95de840bc364aab5a
jbeast291 <75239008+jbeast291@users.noreply.github.com>
2021-01-06 18:58:22 -0800

    Got Event system working but it dosen't do anything yet

---
+++ Assets/scripts/meshActivate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meshActivate : MonoBehaviour
+{
+    [SerializeField] private GameObject myCube;
+
+    public void ActivateCube()
+    {
+        Debug.Log("recived");
+    }
+}
---
+++ Assets/scripts/meshEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class meshEvent : MonoBehaviour
+{
+    [SerializeField] private UnityEvent myTrigger;
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            myTrigger.Invoke();
+            Debug.Log("pad");
+        }
+    }
+}
