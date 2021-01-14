ed417c9b5b49c00d52124a483bf1c11b0df93315
jbeast291 <75239008+jbeast291@users.noreply.github.com>
2021-01-13 18:59:02 -0800

    backuo just in case

--- Assets/scripts/meshActivate.cs
+++ Assets/scripts/meshActivate.cs
@@ -1,13 +1,28 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class meshActivate : MonoBehaviour
 {
     [SerializeField] private GameObject myCube;
 
+    public Material invisable;
+    public Material solidcolor;
+    
+    //[SerializeField] private GameObject Cube;
+
+
     public void ActivateCube()
     {
-        Debug.Log("recived");
+        Debug.Log("visable");
+        myCube.GetComponent<MeshRenderer>().materials[0] = solidcolor;
+    }
+
+
+    public void DeactivateCube()
+    {
+        Debug.Log("invisable");
     }
 }
+ 
